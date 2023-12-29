efc32ae2f7fa06fa899530625a5ec9f3797b94e3
Gabriel Almeida <gabriel.almeida673@outlook.com>
2023-12-28 21:18:32 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Iphone");
+Smartphone iphone1 = new Iphone("933241", "Iphone X", "2323", 64);
+iphone1.ReceberLigacao();
+iphone1.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Nokia");
+Smartphone nokia1 = new Nokia("932123", "Nokia Lumia 930", "4321", 32);
+nokia1.Ligar();
+nokia1.InstalarAplicativo("Facebook");
